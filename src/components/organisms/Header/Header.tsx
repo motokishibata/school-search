95f2b4ccbc863c1df69e9bf0078c7ebec874e8ed
adv_shibata <shibata@adv.co.jp>
2021-10-11 13:46:46 +0900

    link logo

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -2,19 +2,21 @@ import Image from 'next/image';
 import styles from './Header.module.css';
 
 type Props = {
-	icon: StaticImageData,
-	title: string
+  icon: StaticImageData,
+  title: string
 }
 
 const Header = ({icon, title}: Props) => {
-	return (
-		<header className={styles.root}>
-			<div className={styles.logo_container}>
-				<Image src={icon} width={50} height={50} />
-				<h1 className={styles.title}>{title}</h1>
-			</div>
-		</header>
-	);
+  return (
+    <header className={styles.root}>
+      <div className={styles.logo_container}>
+        <a href="/">
+          <Image src={icon} width={50} height={50} />
+        </a>
+        <h1 className={styles.title}>{title}</h1>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
