ab6adabc1f3b55a6a9e0b7745ff3e5fd87f019a5
motokishibata <33312758+motokishibata@users.noreply.github.com>
2021-10-12 02:36:47 +0900

    jsonrized search conditions

--- src/components/atoms/Dropdown/Dropdown.tsx
+++ src/components/atoms/Dropdown/Dropdown.tsx
@@ -3,14 +3,22 @@ import styles from './Dropdown.module.css';
 type Props = {
   id?: string,
   name: string,
-  options: string[][]
+  options: {
+    range: {
+      min: number,
+      max: number
+    },
+    display: string
+  }[]
 };
 
 const Dropdown = ({id, name, options}: Props) => {
   return (
     <div className={styles.root}>
       <select className={styles.select} name={name} id={id}>
-        {options.map(op => <option value={op[0]}>{op[1]}</option>)}
+        {options.map(op => {
+          <option value={op[0]}>{op[1]}</option>
+        })}
       </select>
     </div>
   );
